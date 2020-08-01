86d63153a5f9f6d681b6f8d9f6e47a272808ea05
CamilaKrioka <camilamiki96@gmail.com>
2020-08-01 19:25:33 -0300

    agrego el /

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`Server on : ${port}`)
+  console.log(`Server on : ${port}`);
+});
+app.get("/", (req, res) => {
+  res.json(about);
+});
+
+app.get("/about", (req, res) => {
+  res.send("funciona");
+});
+
+app.get("/portfolio", (req, res) => {
+  res.json(portfolio);
 });
