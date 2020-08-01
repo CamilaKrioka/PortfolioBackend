c53d259e6106e20ff7a8badfbc4c78fa13bb21d4
CamilaKrioka <camilamiki96@gmail.com>
2020-08-01 20:33:20 -0300

    ...

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(cors()); 
 
 const port = process.env.PORT || 8080;
 
+const about = require("./JSON/About.json");
+const portfolio = require("./JSON/Portfolio.json");
 
-
-  const about = require("./JSON/About.json");
-  const portfolio = require("./JSON/Portfolio.json");
-
-
-
-app.get("/", (req, res) => {
-  res.json(about);
+app.get('/', (req, res) => {
+    res.send("Amiga, funciona")
 });
 
-app.get("/about", (req, res) => {
-  res.send("funciona");
+app.get('/about', (req, res) => {
+    res.json(about)
 });
 
-app.get("/portfolio", (req, res) => {
-  res.json(portfolio);
+app.get('/portfolio', (req, res) => {
+    res.json(portfolio)
 });
 
 
 app.listen(port, () => {
-    console.log(`Server on : ${port}`);
-    
+    console.log(`Server on : ${port}`)
 });
